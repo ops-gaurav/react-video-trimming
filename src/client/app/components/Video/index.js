b63f0cf3c8af0bf7dc67acaaf0c131399992d8bb
gaurav <sharma02gaurav@gmail.com>
2019-08-25 23:25:31 +0530

    more cleaning up of custom trimming

--- src/client/app/components/Video/index.js
+++ src/client/app/components/Video/index.js
@@ -5,21 +5,50 @@ import './index.scss';
 
 export default () => {
 	let xPoint = 0;
-	let yPoint = 600;
+	let yPoint = 590;
+
+	const trimLimits = {
+		xPoint: 0,
+		yPoint: 0
+	}
 
 	function drag(event) {
 		event.dataTransfer.setData('id', event.target.id);
 	};
 
+	function fancyTimeFormat(time) {
+		// Hours, minutes and seconds
+		var hrs = ~~(time / 3600);
+		var mins = ~~((time % 3600) / 60);
+		var secs = ~~time % 60;
+
+		// Output like "1:01" or "4:03:59" or "123:03:59"
+		var ret = "";
+
+		if (hrs > 0) {
+			ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+		}
+
+		ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+		ret += "" + secs;
+		return ret;
+	}
+
 	function drop(event) {
 		event.preventDefault();
 		const data = event.dataTransfer.getData('id');
 		// console.log(data);
-		// const vid = document.getElementById("video");
+		const vid = document.getElementById("video");
+		const division = (vid.duration / document.getElementById('marker').clientWidth);
 		const clickedPosition = event.pageX;
 		const bounds = document.getElementById('marker').getBoundingClientRect();
 		// console.log(clickedPosition - bounds.left);
 		const componentPosition = clickedPosition - bounds.left;
+		const time = componentPosition * division;
+		// console.log(fancyTimeFormat(time));
+		trimLimits[data] = fancyTimeFormat(time);
+		document.getElementById(`${data}Value`).innerHTML = trimLimits[data];
+		console.log(trimLimits);
 		document.getElementById(data).style.marginLeft = `${componentPosition}px`;
 	};
 	return <div>
@@ -49,18 +78,18 @@ export default () => {
 			<span
 				draggable={true}
 				className='selectors'
-				style={{ marginLeft: `${xPoint}px`}}
+				style={{ marginLeft: `${xPoint}px` }}
 				onDragStart={drag}
 				id='xPoint'>
-				<FontAwsome name='caret-left' />
+				<FontAwsome name='caret-left' />&nbsp;&nbsp;<span className='time' id='xPointValue'>{trimLimits.xPoint}</span>
 			</span>
 			<span
 				draggable={true}
 				className='selectors'
-				style={{ marginLeft: `${yPoint}px`}}
+				style={{ marginLeft: `${yPoint}px` }}
 				onDragStart={drag}
 				id='yPoint'>
-				<FontAwsome name='caret-right' />
+				<FontAwsome name='caret-right' />&nbsp;&nbsp;<span className='time' id='yPointValue'>&nbsp;{trimLimits.yPoint}</span>
 			</span>
 		</section>
 	</div>;
